40a0d31a8a72d6a7173ca5395bbf01781c8c4d70
siriuseddy <siriusgundam@gmail.com>
2015-03-08 21:37:32 +0800

    add spring-mvc

---
+++ spring-mvc/src/main/java/com/jdon/spring/controller/HelloWorldController.java
@@ -0,0 +1,19 @@
+package com.jdon.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HelloWorldController {
+
+	@RequestMapping("/hello")
+	public String hello(
+			@RequestParam(value = "name", required = false, defaultValue = "World") String name,
+			Model model) {
+		model.addAttribute("name", name);
+		return "helloworld";
+	}
+
+}
