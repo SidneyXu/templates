c3e07faa680ed4657160b330126935c420cc033b
siriuseddy <siriusgundam@gmail.com>
2015-04-29 08:11:48 +0800

    update build.gradle

---
+++ gradle-android/example-android-lib/src/main/java/com/example/android/lib/Bean.java
@@ -0,0 +1,25 @@
+package com.example.android.lib;
+
+/**
+ * Created by mrseasons on 2015/4/28.
+ */
+public class Bean {
+
+    private String privateField;
+    public String publicField;
+    String friendlyField;
+
+    private void privateMethod() {
+
+    }
+
+    public void publicMethod() {
+    }
+
+    void friendlyMethod() {
+    }
+
+    class InnerClass {
+
+    }
+}
