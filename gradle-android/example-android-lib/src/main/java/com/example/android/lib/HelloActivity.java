03000d6c7a0e7d1cbe3616705b79de781b6b0b9c
sidneyy <siriuseddy@gmail.com>
2015-07-24 18:30:46 +0800

    update

--- gradle-android/example-android-lib/src/main/java/com/example/android/lib/HelloActivity.java
+++ gradle-android/example-android-lib/src/main/java/com/example/android/lib/HelloActivity.java
@@ -1,10 +1,7 @@
 package com.example.android.lib;
 
-import android.app.Activity;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
 import android.widget.TextView;
 import com.example.java.lib.Person;
 
@@ -18,13 +15,13 @@ public class HelloActivity extends FragmentActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        int id =getResources().getIdentifier("text", "id", getPackageName());
+        int id = getResources().getIdentifier("text", "id", getPackageName());
         TextView textView = (TextView) findViewById(id);
         Person person = new Person("Tom");
         textView.setText(person.hello());
     }
 
-    protected void createFragment(){
+    protected void createFragment() {
 
     }
 }
