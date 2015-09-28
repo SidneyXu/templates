6216ed651c0cb89f2cd9c4a1d2513cd9d5a2df14
sidneyy <siriuseddy@gmail.com>
2015-09-28 18:21:43 +0800

    Update.

--- hello-jni/app/src/main/jni/com_example_jni_Hello.cpp
+++ hello-jni/app/src/main/jni/com_example_jni_Hello.cpp
@@ -1,10 +1,10 @@
-#include <com_example_jni_Hello.h>
+#include "com_example_jni_Hello.h"
 /*
  * Class:     com_example_jni_Hello
  * Method:    hellojni
  * Signature: ()Ljava/lang/String;
  */
 JNIEXPORT jstring JNICALL Java_com_example_jni_Hello_hellojni
-  (JNIEnv *env, jobject){
-    return env->NewStringUTF( "hello from jni");
-  }
+        (JNIEnv *env, jobject) {
+    return env->NewStringUTF("hello from jni");
+}
--- hello-jni/jni-support/src/main/java/com/example/jni/jni_support/Calculator.java
+++
@@ -1,9 +0,0 @@
-package com.example.jni.jni_support;
-
-/**
- * Created by mrseasons on 2015/09/23.
- */
-public class Calculator {
-
-    public native int add(int x, int y);
-}
